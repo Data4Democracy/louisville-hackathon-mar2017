0a913d270cd3cfbcc249678d25a075537cbb6b0c
Chip Lynch <chip@chiplynch.com>
2017-03-31 20:38:57 -0400

    Initial table creation

---
+++ postgresql/create_structures_script.sql
@@ -0,0 +1,16 @@
+# V0.0.0.0.0.1.  Most everything is a varchar(100) just to test loading.
+# nothing is normalized.
+# iterate iterate iterate!
+
+create table Members (
+    ID bigserial primary key,
+    FirstName varchar(100) not null,
+    LastName varchar(100) not null,
+    District varchar(100) not null,
+    Party varchar(100) not null,
+    Race varchar(100) null,
+    Gender varchar(100) null,
+    FirstElectedYear varchar(100) null,
+    DateOfBirth varchar(100) null
+);
+
