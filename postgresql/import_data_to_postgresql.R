f18c46de81c698fdf93379011812c81ed38cc9e9
Chip Lynch <chip@chiplynch.com>
2017-04-01 12:16:24 -0400

    Create import_data_to_postgresql.R
    New file to pull from the github "raw" csv files and populate postgresql.
    Some manipulation (gather/melt/renaming) is happening when needed.

---
+++ postgresql/import_data_to_postgresql.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(RPostgreSQL)
+
+pw <- {'SETTHEPASSWORDHERE'}
+drv <- dbDriver('PostgreSQL')
+con <- dbConnect(drv,
+                 dbname = 'data4lou', 
+                 host='data4dem4lou.cba3kk0t9rmq.us-east-1.rds.amazonaws.com',
+                 user='iheartpower', password = pw)
+dbExistsTable(con, 'members')
+dbGetQuery(con, 'select * from members')
+dbGetQuery(con, 'select * from ky_ga_bill_votes')
+
+
+# Process House Votes
+  HouseVotes <- read.delim(url('https://raw.githubusercontent.com/Data4Democracy/louisville-hackathon-mar2017/master/House_Votes.csv'), 
+                  sep = ';')
+  votes <- gather(HouseVotes, key=Name)
+  names(votes) <- c('MemberName', 'BillName', 'Vote')
+  dbWriteTable(con, name='votes', value=votes);
+
+  # Process Legislators
+  Legislators <- read.csv(url('https://raw.githubusercontent.com/Data4Democracy/louisville-hackathon-mar2017/master/Legislators.csv'), encoding='UTF-8')
+  names(Legislators)[1] <- c('legislator_id')
+  head(Legislators)
+  dbWriteTable(con, name='Legislators', value=Legislators, overwrite = TRUE);
+   
+# Process Legislature
+  Legislature <- read.csv(url('https://raw.githubusercontent.com/Data4Democracy/louisville-hackathon-mar2017/master/Legislature.csv'))
+  head(Legislature)
+  dbWriteTable(con, name='Legislature', value=Legislature, overwrite = TRUE);
+  
+  
+# Process Session
+  Session <- read.csv(url('https://raw.githubusercontent.com/Data4Democracy/louisville-hackathon-mar2017/master/Session.csv'))
+  head(Session)
+  dbWriteTable(con, name='Session', value=Session, overwrite = TRUE);
